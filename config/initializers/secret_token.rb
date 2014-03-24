3fb52c16aa400020177391aaec42f5a516b7c71a
spk <spk@youhou.(none)>
2014-03-24 12:22:41 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupjeune::Application.config.secret_key_base = '8f51890b7c7a10f46b692c0b6525775736268a59dc0bc8b6bc8f091852d57fb734dc8b444b2bdd4ecf9a003b47d98d487bad9ac949a6694056c5be3b2c204d71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Groupjeune::Application.config.session_store :cookie_store, key: '_groupjeune_session'
